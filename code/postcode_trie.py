cc2b854bbb5cd6ca13724b74962867e46049e351
zim <zimuzostanley@gmail.com>
2016-08-30 14:33:58 +0100

    Feature. Included rural areas. Better location retrieval

--- code/postcode_trie.py
+++ code/postcode_trie.py
@@ -3,17 +3,39 @@ import csv
 import string
 
 
-def get_postcodes_tuple_from_csv(csv_filename, location=[], location_dict={}):
+def get_urban_postcodes_tuple_from_csv(csv_filename, location=[], location_dict={}):
     with open(csv_filename) as csv_file:
         reader = csv.DictReader(csv_file)
         for row in reader:
             if len(row['postcode']) != 6:
                 continue
-            location_dict[row['postcode']] = row['town'].lower() + ', ' + row['state'].lower()
+            location_dict[row['postcode'] + '-' + row['street'].lower()] = row['street'].lower() + ', ' \
+                                                                           + row['area'].lower() + ', ' \
+                                                                           + row['town'].lower() + ', ' \
+                                                                           + row['state'].lower()
+            location_dict[row['postcode'] + '-' + row['area'].lower()] = row['area'].lower() + ', ' \
+                                                                         + row['town'].lower() + ', ' \
+                                                                         + row['state'].lower()
             
             item = (unicode(row['street'].lower(), 'utf-8'), row['postcode'])
             location.append(item)
             item = (unicode(row['area'].lower(), 'utf-8'), row['postcode'])
+            location.append(item)
+    return location, location_dict
+
+
+def get_rural_postcodes_tuple_from_csv(csv_filename, location=[], location_dict={}):
+    with open(csv_filename) as csv_file:
+        reader = csv.DictReader(csv_file)
+        for row in reader:
+            if len(row['postcode']) != 6:
+                continue
+            location_dict[row['postcode'] + '-' + row['town'].lower()] = row['town'].lower() + ', ' + row['district'].lower() + ', ' + row['lga'].lower() + ', ' + row['state'].lower()
+            location_dict[row['postcode'] + '-' + row['district'].lower()] = row['district'].lower() + ', ' + row['lga'].lower() + ', ' + row['state'].lower()
+            
+            item = (unicode(row['town'].lower(), 'utf-8'), row['postcode'])
+            location.append(item)
+            item = (unicode(row['district'].lower(), 'utf-8'), row['postcode'])
             location.append(item)
     return location, location_dict
 
@@ -28,10 +50,11 @@ def retrieve_by_prefix(trie, prefix):
 def format_result(pre_result, location_dict):
     result = []
     for pr in pre_result:
-        postcode = string.join(pr[1])
-        postcode = postcode.replace(' ', '')
-        other_address = location_dict[postcode]
-        result.append(pr[0] + ', ' + other_address + ' - ' + postcode)
+        postcode = string.join(pr[1]).replace(' ', '')
+        other_address = location_dict.get(postcode + '-' + pr[0])
+        print other_address
+        if other_address is not None:
+            result.append(other_address + ' - ' + postcode)
     return result
 
 # locations, location_dict = get_postcodes_tuple_from_csv('../data/urban_postcodes.csv')
--- code/postcode_web.py
+++ code/postcode_web.py
@@ -3,7 +3,8 @@ import postcode_trie as pt
 import json
 
 
-locations, location_dict = pt.get_postcodes_tuple_from_csv('../data/urban_postcodes.csv')
+locations, location_dict = pt.get_urban_postcodes_tuple_from_csv('../data/urban_postcodes.csv')
+locations, location_dict = pt.get_rural_postcodes_tuple_from_csv('../data/rural_postcodes.csv', locations, location_dict)
 trie = pt.build_trie(locations)
 
 
@@ -17,6 +18,8 @@ def index():
 def get_postcodes():
     street = request.args.get('term')
     if street is not None:
+        if len(street) < 3:
+            return json.dumps([])
         retrieved = pt.retrieve_by_prefix(trie, street)
         formatted = pt.format_result(retrieved, location_dict)
         if len(formatted) == 0:
